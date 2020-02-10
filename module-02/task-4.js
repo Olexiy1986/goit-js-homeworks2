6a0c65e6303dc57f34c0862db8a0578649c1982f
Oleksiy Shovkun <ssapfir1986@ukr.net>
2020-02-10 19:57:01 +0300

    two

---
+++ module-02/task-4.js
@@ -0,0 +1,24 @@
+// 'use strict';
+
+// const formatString = function(string) {
+//   const originString = string;
+//   const formating = originString.slice(0, 40);
+
+//   if (string.length < 40) {
+//     console.log(originString);
+//   } else {
+//     console.log(`${formating} ...`);
+//   }
+// };
+
+// formatString('Curabitur ligula sapien, tincidunt non.');
+// // вернется оригинальная строка
+
+// formatString('Vestibulum facilisis, purus nec pulvinar iaculis.');
+// // вернется форматированная строка
+
+// formatString('Curabitur ligula sapien.');
+// // вернется оригинальная строка
+
+// formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.');
+// // вернется форматированная строка
---
+++ module-02/task-5.js
@@ -0,0 +1,27 @@
+// 'use strict';
+
+// const checkForSpam = function(message) {
+//     const spam = 'spam';
+//     const sale = 'sale';
+//     const spamArr = message.toLowerCase();
+
+//     for (let i = 0; i < message.length; i += 1) {
+//         if(message.includes(sale) || spamArr.includes(spam)) {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+
+
+
+
+
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
