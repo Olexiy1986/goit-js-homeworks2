4e79168fed25319c0fac506a0668ef8b15c52d07
Oleksiy Shovkun <ssapfir1986@ukr.net>
2020-03-12 19:07:06 +0300

    five

---
+++ module-05/task-1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Account = function({login, email}) {
+  this.login = login;
+  this.email = email;
+};
+
+Account.prototype.getInfo = function() {
+  console.log(`login: ${this.login}, email: ${this.email}`);
+};
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
